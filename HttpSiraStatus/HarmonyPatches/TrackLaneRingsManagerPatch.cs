5b929fb56341f1f536f3318ec802ba66521dc707
denpadokei <jsoldenpa@yahoo.co.jp>
2023-11-20 23:30:22 +0900

    カスタムプラットフォームが死なないようにするやつ

---
+++ HttpSiraStatus/HarmonyPatches/TrackLaneRingsManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace HttpSiraStatus.HarmonyPatches
+{
+    /// <summary>
+    /// カスタムプラットフォームが死なないようにするやつ
+    /// </summary>
+    [HarmonyPatch(typeof(TrackLaneRingsManager), MethodType.Constructor)]
+    internal class TrackLaneRingsManagerPatch
+    {
+        public static void Postfix(ref TrackLaneRing[] ____rings)
+        {
+            if (____rings == null) {
+                ____rings = new TrackLaneRing[0];
+            }
+        }
+    }
+}
